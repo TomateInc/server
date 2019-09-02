42e710fde00ea3feeb39aaccb10b426cc5ab88f5
Colman Joeri <colmanjoeri@msn.com>
2019-09-02 16:14:22 +0200

    start on mqtt stuff

--- devices/light/mqtt.js
+++ devices/light/mqtt.js
@@ -1,8 +1,18 @@
+const MQTT = require("async-mqtt");
+
 module.exports = {
-	create: function (config) {
+	create: async function (config) {
 		//subscribe to mqtt messages to update the state
-		//todo
+		//todo implement config
 		// respond to mqtt state changes
+		const client = await MQTT.connectAsync("tcp://somehost.com:1883")
+
+		await client.subscribe('home/rgb1');
+
+		client.on('message', function (topic, message) {
+			// message is Buffer
+			console.log(message.toString());
+		});
 
 		return {
 			config,
@@ -11,11 +21,13 @@ module.exports = {
 			set: function(state) {
 				//set the current state
 				//todo send command to the light
+				//await client.publish("wow/so/cool", "It works!");
 			},
 
 			toggle: function() {
 				//toggle action
 				//todo send command to the light
+				//await client.publish("wow/so/cool", "It works!");
 			},
 		};
 	},
