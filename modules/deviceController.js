e40439efb5909f840c78327eaefc1bcdb18b524c
depuits <colmanjoeri@gmail.com>
2019-12-24 13:05:05 +0100

    exposed addDevice function in dev ctrl

--- modules/deviceController.js
+++ modules/deviceController.js
@@ -1,32 +1,43 @@
 'use strict';
 
+const eventEmitter = require('events').EventEmitter;
+
 module.exports = async (config) => {
 	console.log('creating device controller');
 	const devconfigs = config.get('devices'); // todo get devices from other place such as a dedicated config file or db
 	
+	let controller = Object.create(new eventEmitter());
 	let devices = [];
 
-	return { 
+	async function addDevice(app, conf) {
+		let devType = require(`../devices/${conf.type}`);
+		let newDev = await devType(app, conf);
+
+		newDev.id = conf.id;
+		newDev.name = conf.name;
+		newDev.type = conf.type;
+
+		devices.push(newDev);
+
+		return newDev;
+	}
+
+	return Object.assign(controller, {
 		async initialize(app) {
 			console.log('creating devices');
 			for (let d of devconfigs) {
 				try {
-					let devType = require(`../devices/${d.type}`);
-					let newDev = await devType(app, d);
-
-					newDev.id = d.id;
-					newDev.name = d.name;
-					newDev.type = d.type;
-
-					devices.push(newDev);
+					await addDevice(app, d);
 				} catch (e) {
 					console.error(`Failed to load device (${d.id}): ${e}`);
 				}
 			}
 		},
+		addDevice,
 		stateChange(devId, dev) {
-			//TODO add event emitter so we can tell other components about this
+			// tell other components about this change
+			controller.emit('stateChange', devId, dev, controller);
 		},
 		devices,
-	};
+	});
 };
